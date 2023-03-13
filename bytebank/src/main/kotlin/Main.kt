891d28c725c91d32dc8ee4450bb282905450a627
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-13 12:23:18 -0300

    Criacao da funcao que testa condicoes do saldo

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -11,6 +11,10 @@ fun main() {
     println("número da conta $numeroConta")
     println("saldo da conta $saldo")
 
+    testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
     if (saldo > 0.0) {
         println("conta é positiva")
     } else if (saldo == 0.0){
