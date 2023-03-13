067799ded7715c182cd64b0bcab1ab966a05b990
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-13 12:36:25 -0300

    Adicionado estruturas de repeticao

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,23 +1,40 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Vinícius"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 1000
+    for (i in 1..5) {
+        val titular: String = "Vinícius $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = 0.0
+        saldo = i + 10.0
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
 
-    testaCondicoes(saldo)
+    for (i in 4 downTo 1 step 2) {
+        if (i == 3) {
+            break
+        } else if (i == 1) {
+            continue
+        }
+
+        println("valor do i: $i")
+    }
+
+    var valorWhile = 1
+    while (valorWhile < 4) {
+        println("valor while: $valorWhile")
+        valorWhile++
+    }
+
+    //testaCondicoes(saldo)
 }
 
 fun testaCondicoes(saldo: Double) {
     if (saldo > 0.0) {
         println("conta é positiva")
-    } else if (saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("conta é neutra")
     } else {
         println("conta é negativa")
