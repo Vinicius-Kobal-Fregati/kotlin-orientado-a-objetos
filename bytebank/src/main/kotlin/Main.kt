842f5d926dc0f1c43ce3a801823a67cc95784391
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-13 12:19:09 -0300

    Utilizacao de estruturas condicionais e do when

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -5,8 +5,26 @@ fun main() {
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
+    saldo -= 1000
 
     println("titular $titular")
     println("número da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0){
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    // Mesma coisa que o if, ele é semelhante ao switch do Java
+    /*
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+    */
 }
