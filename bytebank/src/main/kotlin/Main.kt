a18bd9dd39f579af5581ea8a97011507d89e54a3
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-14 11:50:09 -0300

    Tentativa de implementar funcionario admin

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    testaComportamentosConta()
-    testaCopiasEReferencias()
-    testaContasDiferentes()
+
+    val gerente = Gerente(
+        nome = "Vinícius",
+        cpf = "111.111.111-11",
+        salario = 3000.0,
+        senha = 1234
+    )
+
+    val diretora = Diretor(
+        nome = "Fran",
+        cpf = "111.111.111-12",
+        salario = 5000.0,
+        senha = 1234,
+        plr = 200.0
+    )
+
+    val sistema = SistemaInterno()
+    sistema.entra(gerente, 1235)
+    sistema.entra(diretora, 1234)
 }
