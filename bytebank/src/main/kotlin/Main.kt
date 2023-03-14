6f98e97bc38193879469b19e601d2b42cbc8e625
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-13 21:32:43 -0300

    Criacao da classe funcionario

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    testaComportamentosConta()
+
+    val vinicius = Funcionario(
+        nome = "Vinícius",
+        cpf = "111.111.111-11",
+        salario = 2000.0
+    )
+
+    println("nome ${vinicius.nome}")
+    println("cpf ${vinicius.cpf}")
+    println("salario ${vinicius.salario}")
+    println("bonificação ${vinicius.bonificacao()}")
 }
 
