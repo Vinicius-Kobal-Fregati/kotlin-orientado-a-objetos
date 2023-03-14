42e7ffb30e6f2c2b902962d5622e215d6820f942
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-13 21:23:54 -0300

    Organizacao do projeto

---
+++ bytebank/src/main/kotlin/Conta.kt
@@ -0,0 +1,50 @@
+class Conta(
+    var titular: String,
+    val numero: Int = 0
+) {
+    var saldo = 0.0
+        private set
+    /*
+        set(valor) {
+            // field é o valor interno da property
+            if (valor > 0) {
+                field = valor
+            }
+        }
+     */
+
+    // Executa um trecho de código durante a inicialização
+    init {
+        println("Conta do(a) $titular criada")
+    }
+
+    /*
+    //Construtor secundário
+    constructor(titular: String, numero: Int) {
+        this.titular = titular
+        this.numero = numero
+    }
+     */
+
+    fun deposita(valor: Double) {
+        println("Depositando na conta do(a) $titular")
+        this.saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            println("Sacando na conta do(a) $titular")
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            println("Transferência da conta do(a) $titular para o(a) ${destino.titular}")
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+}
---
+++ bytebank/src/main/kotlin/testaCondicoes.kt
@@ -0,0 +1,18 @@
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    // Mesma coisa que o if, ele é semelhante ao switch do Java
+    /*
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+    */
+}
