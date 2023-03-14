845127c20b3f6aa90ddaca80118672deada3a5da
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-14 10:32:06 -0300

    Aplicacao do polimorfismo

--- bytebank/src/main/kotlin/CalculadoraBonificacao.kt
+++ bytebank/src/main/kotlin/CalculadoraBonificacao.kt
@@ -6,12 +6,4 @@ class CalculadoraBonificacao {
     fun registra(funcionario: Funcionario) {
         this.total += funcionario.bonificacao
     }
-
-    fun registra(funcionario: Gerente) {
-        this.total += funcionario.bonificacao
-    }
-
-    fun registra(funcionario: Diretor) {
-        this.total += funcionario.bonificacao
-    }
 }
