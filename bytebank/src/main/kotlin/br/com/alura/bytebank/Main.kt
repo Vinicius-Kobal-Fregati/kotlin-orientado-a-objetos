98502cc93ffbce9d005c8d850eb78e483b5a4044
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-15 11:04:18 -0300

    Introducao a exceptions

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,5 +1,7 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
     println("Bem vindo ao Bytebank")
 
@@ -18,6 +20,8 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5) {
         println(i)
+        val endereco = Any()
+        endereco as Endereco
     }
     println("fim funcao2")
 }
