146f628a7b472677561bf8d7befa9c953566e1ff
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-15 16:00:23 -0300

    Testando o safe call

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -4,10 +4,30 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
     println("Bem vindo ao Bytebank")
-    var enderecoNulo: Endereco? = null
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+
+    // O let coloca nosso objeto dentro de uma scope function
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+    }
+
+    /*
+    // Outras formas de usar o let
+    enderecoNulo?.let {endereco ->
+        println(endereco.logradouro.length)
+    }
+
+    // Encadeamento de safe call
+    enderecoNulo.let {endereco: Endereco? ->
+        println(endereco?.logradouro?.length)
+    }
+    */
+
+    /*
     // Apenas pega o campo se não for nulo
     enderecoNulo?.logradouro
     //Garantimos que o objeto não é nulo
     val enderecoNaoNulo: Endereco = enderecoNulo!!
     enderecoNaoNulo.logradouro
+     */
 }
