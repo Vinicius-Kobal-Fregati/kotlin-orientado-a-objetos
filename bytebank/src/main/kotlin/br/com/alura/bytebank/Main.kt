054359ff7735bbe4c6ca0cee92d4dd5e504f77a9
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-14 21:18:02 -0300

    Utilizando object expressions para criar um objeto anonimo

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,12 +1,24 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
 fun main() {
     println("Bem vindo ao Bytebank")
+
+    // Object expression para criar um objeto anônimo
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "222.222.222-22"
+        val senha: Int = 1003
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("nome do cliente ${fran.nome}")
 
     val vinicius = Cliente(nome = "Vinícius", cpf = "", senha = 1)
     var contaPoupanca = ContaPoupanca(titular = vinicius, numero = 1000)
