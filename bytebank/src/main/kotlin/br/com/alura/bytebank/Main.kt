c30a15779e13f4d8f6505ce239dc0951070a9a93
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-15 13:43:22 -0300

    Utilizando try, if e when expression

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -6,6 +6,38 @@ fun main() {
     println("Bem vindo ao Bytebank")
 
     println("início main")
+    val entrada: String = "1,9"
+
+    // Usando o try como expressão (try-expression)
+    val valorRecebido: Double? = try {
+        // retorna sempre a última linha
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    // Usando if como expressão (if-expression)
+    val valorComTaxa: Double? = if(valorRecebido != null) {
+        // retorna sempre a última linha
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    // When expression
+    val possuiTaxa: Boolean = when {
+        valorComTaxa != null -> true
+        else -> false
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+
     funcao1()
     println("fim main")
 }
