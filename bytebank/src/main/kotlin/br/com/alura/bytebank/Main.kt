6a7277841c9b07a0712c1ffeb664b8022bdd1c7b
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-23 10:02:55 -0300

    Conhecendo o paradigma funcional

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,49 +1,15 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Endereco
-
+var numero = 10
 fun main() {
     println("Bem vindo ao Bytebank")
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
-
-    // O let coloca nosso objeto dentro de uma scope function
-    enderecoNulo?.let {
-        println(it.logradouro.length)
-        // Elvis operator, se for nulo, retornará o valor após o ?:
-        val tamanhoComplemento: Int = it.complemento?.length ?: 0
-        println(tamanhoComplemento)
-    }
-
-    val tamanhoComplemento: Int = try {
-        // Podemos usar o elis também para lançar exceção
-        enderecoNulo?.complemento?.length ?: throw IllegalStateException()
-    } catch (e: IllegalStateException) {
-        println("Complemento não pode ser vazio")
-        0
-    }
-    println(tamanhoComplemento)
-
-    // Outras formas de usar o let
-    enderecoNulo?.let { endereco ->
-        println(endereco.logradouro.length)
-    }
-
-    // Encadeamento de safe call
-    enderecoNulo.let { endereco: Endereco? ->
-        println(endereco?.logradouro?.length)
-    }
-
-    /*
-    // Apenas pega o campo se não for nulo
-    enderecoNulo?.logradouro
-    //Garantimos que o objeto não é nulo
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
-     */
-    teste("a")
+    println(soma(1, 2))
+    numero = 3
+    println(soma(1, 2))
 }
 
-fun teste(valor: Any) {
-    val numero: Int? = valor as? Int
-    println(numero)
-}
+// Isso se assemelha mais à orientação a objetos, veja que mantém estado.
+fun soma(a: Int, b: Int) : Int = a + b + numero
+
+// Essa função se assemelha mais à funcional, ela é pura, ou seja, para mesmos argumentos o retorno será igual
+fun somaFuncional(a: Int, b: Int) : Int = a + b
