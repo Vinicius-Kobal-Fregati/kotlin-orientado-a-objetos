7b1d882eae17c90b2c400272b1ad6c6b4d954ae8
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-23 11:46:06 -0300

    Lambda e funcao anonima com parametro e retorno

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -9,31 +9,59 @@ fun main() {
 //    println(somaNaoFuncional(1, 2))
 //    println(somaFuncional(1, 2))
 
-    testaTipoFuncaoReferencia()
-    testaTipoFuncaoClasse()
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
 //    testaOperadorInvoke()
 
-    /*
     // Expressão lambda
-    val minhaFuncaoLambda: () -> Unit = {
+    // Normalmente entre a função anônima e essa, a expressão lambda é mais usada
+    val minhaFuncaoLambda: (Int, Int) -> Int = { a, b ->
         println("Executa como lambda")
+        // Lembrando que a última linha é o retorno
+        a + b
     }
+
+    // Quando não for usar algum parâmetro, pode usar o _
+    val outraFuncaoLambda: (Int, Int) -> Int = { _, b ->
+        println("Executa como lambda")
+        b
+    }
+
+    val maisUmaFuncaoLambda = { a: Int , b: Int ->
+        println("Executa como lambda")
+        // Lembrando que a última linha é o retorno
+        a + b
+    }
+
     println(minhaFuncaoLambda)
-    println(minhaFuncaoLambda())
+    println(minhaFuncaoLambda(15, 10))
 
     // Expressão anônima
-    // Tanto essa como a lambda não podem ser reutilizadas em outra variável
-    val minhaFuncaoAnonima: () -> Unit = fun() {
+    // Tanto essa como a lambda não podem ser reutilizadas em outra variável.
+    // Usamos ela em situações que queremos deixar bem claro os parâmetros e retornos
+    // Além do escopo de execução do código ser mais claro (obrigatório o return).
+    // É uma forma mais explícita da função lambda
+    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a, b): Int {
         println("Executa como anônimo")
+        return a + b
+    }
+
+    val outraFuncaoAnonima = fun(a: Int, b: Int): Int {
+        println("Executa como anônimo")
+        return a + b
+    }
+
+    val maisUmaFuncaoAnonima = fun(_: Int, b: Int): Int {
+        println("Executa como anônimo")
+        return b
     }
     println(minhaFuncaoAnonima)
-    println(minhaFuncaoAnonima())
+    println(minhaFuncaoAnonima(20, 5))
 
     /*
     // Esses dois casos não funcionam, elas não podem ser reutilizadas em outras variáveis
     val reutilizandoLambda: () -> Unit = ::minhaFuncaoLambda
     val reutilizandoAnonimo: () -> Unit = ::minhaFuncaoAnonima
-     */
      */
 }
 
@@ -64,7 +92,7 @@ private fun testaTipoFuncaoReferencia() {
     println(minhaFuncao(1, 2))
 }
 
-fun soma(a: Int, b: Int) : Int {
+fun soma(a: Int, b: Int): Int {
     return a + b
 }
 
