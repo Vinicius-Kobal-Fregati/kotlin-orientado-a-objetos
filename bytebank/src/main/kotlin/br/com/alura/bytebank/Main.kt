284c03d24ed6f4003d22c6e10cb0966a22d98ec0
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-25 10:19:29 -0300

    Testando outras funcoes de escopo, como apply, also, run e with

--- bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -7,9 +7,33 @@ import br.com.alura.bytebank.modelo.SistemaInterno
 fun main() {
     println("Bem vindo ao Bytebank")
     // Com Higher-order function não precisaríamos dessa variável
-//    val endereco = Endereco(logradouro = "rua vergueiro", numeroEndereco = 3185)
-//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numeroEndereco}".uppercase()
+    val enderecoVariavel = Endereco(logradouro = "rua vergueiro", numeroEndereco = 3185)
+
+    // O apply nos permite trabalhar com os parâmetros do objeto
+    // Utiliza um receiver (this) e retorna o objeto de contexto
+    val enderecoVariavelComApply = Endereco()
+        // Disponibiliza o contexto do objeto como argumento (it). O retorno é o objeto de contexto
+        // Amplamente usado em ações que não impactam o restante do código, como impressões.
+        .also {
+            println("Criando endereço")
+        }
+        .apply {
+            logradouro = "rua vergueiro"
+            numeroEndereco = 3185
+        }
+//    val enderecoEmMaiusculo = "${enderecoVariavel.logradouro}, ${enderecoVariavel.numeroEndereco}".uppercase()
 //    println(enderecoEmMaiusculo)
+
+    // Pode ser usado como extensão ou não
+    // Normalmente é utilizado na inicialização de objetos junto com computação no final,
+    // ou apenas para computar algo sem relação com objeto
+    run {
+        println("Execução do run sem extensão")
+    }
+
+    with(enderecoVariavel) {
+        "${logradouro}, ${numeroEndereco}".uppercase()
+    }.let(::println)
 
     val enderecoEmMaiusculo = Endereco(
         logradouro = "rua vergueiro",
@@ -68,7 +92,7 @@ fun main() {
         println(it)
     })
 
-    val autenticavel = object: Autenticavel {
+    val autenticavel = object : Autenticavel {
         val senha = 1234
         override fun autentica(senha: Int): Boolean {
             return this.senha == senha
